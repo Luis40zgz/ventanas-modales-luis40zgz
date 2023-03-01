cc1112fd98775c42b27be0a7594d52f51d381f62
luis40 <luissg21@gmail.com>
2023-03-01 13:49:35 +0100

    Todas las funcionalidades implementadas

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+//Capturo los elementos
 let buttons = document.querySelectorAll('.show-modal')
+let cross = document.querySelector('.close-modal')
+let modal = document.querySelector('.modal')
+let overlay = document.querySelector('.overlay')
+
+//Asigno los listeners
+buttons.forEach((element) => element.addEventListener('click', showModal))
+cross.addEventListener('click', hideModal)
+overlay.addEventListener('click', hideModal)
+document.addEventListener('keydown', hideModalEsc)
+
+//Genero las funciones
 function showModal() {
-  let modal = document.querySelector('.modal')
   modal.style.display = 'initial'
+  overlay.style.display = 'initial'
 }
-buttons.forEach((element) => {
-  element.addEventListener('click', showModal)
-})
+function hideModal() {
+  modal.style.display = 'none'
+  overlay.style.display = 'none'
+}
+function hideModalEsc(e) {
+  if (e.key === 'Escape') {
+    modal.style.display = 'none'
+    overlay.style.display = 'none'
+  }
+}
